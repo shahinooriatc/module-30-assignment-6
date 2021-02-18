59a5fe71ca16858f7e44fa98aa5be9564ce76794
shahinooriact <shahinooriact@gmail.com>
2021-02-19 00:36:45 +0600

    Code organized first step

--- app.js
+++ app.js
@@ -4,14 +4,37 @@ const galleryHeader = document.querySelector('.gallery-header');
 const searchBtn = document.getElementById('search-btn');
 const sliderBtn = document.getElementById('create-slider');
 const sliderContainer = document.getElementById('sliders');
-// selected image 
-let sliders = [];
-
 
 // If this key doesn't work
 // Find the name in the url and go to their website
 // to create your own api key
 const KEY = '15674931-a9d714b6e9d654524df198e00&q';
+
+// Click Search Button for Data..........
+searchBtn.addEventListener('click', function () {
+  document.querySelector('.main').style.display = 'none';
+  clearInterval(timer);
+  const search = document.getElementById('search');
+  getImages(search.value)
+  sliders.length = 0;
+})
+
+// Load data from API
+const getImages = (query) => {
+  fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
+    .then(response => response.json())
+    .then(data => showImages(data.hits))
+    .catch(err => console.log(err))
+}
+
+// Enter Key Work for ... Search box
+let input = document.getElementById("search");
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("search-btn").click();
+    }
+});
 
 // show images 
 const showImages = (images) => {
@@ -25,18 +48,13 @@ const showImages = (images) => {
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div)
   })
-
 }
 
-const getImages = (query) => {
-  fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
-    .then(response => response.json())
-    .then(data => showImages(data.hits))
-    .catch(err => console.log(err))
-}
+// selected image 
+let sliders = [];
 
 let slideIndex = 0;
-const selectItem = (event, img) => {
+const selectItem = (event,img) => {
   let element = event.target;
   element.classList.add('added');
  
@@ -106,23 +124,6 @@ const changeSlide = (index) => {
 
   items[index].style.display = "block"
 }
-
-searchBtn.addEventListener('click', function () {
-  document.querySelector('.main').style.display = 'none';
-  clearInterval(timer);
-  const search = document.getElementById('search');
-  getImages(search.value)
-  sliders.length = 0;
-
-})
-// Enter Key Work for ... Search box
-let input = document.getElementById("search");
-input.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("search-btn").click();
-    }
-});
 
 sliderBtn.addEventListener('click', function () {
   createSlider()
